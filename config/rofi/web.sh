5724ce2b4b5c76b0cdb4a8dbdd7937eeb8acb528
josep <josepm.ramos.i@gmail.com>
2025-12-31 14:05:17 +0100

    dunst and web rofi script

---
+++ config/rofi/web.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declare -A LINKS=(
+  ["whatsapp"]="https://web.whatsapp.com"
+  ["chatgpt"]="https://chat.openai.com"
+  ["campus"]="https://campusvirtual.uclm.es"
+  ["github"]="https://github.com"
+  ["youtube"]="https://youtube.com"
+)
+choice=$(printf "%s\n" "${!LINKS[@]}" | rofi -dmenu -i -p "Web")
+[[ -n "$choice" ]] && xdg-open "${LINKS[$choice]}"
+
