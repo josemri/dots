4e69a8dc706a962352893ce64dba6c81b01c884a
josep <josepm.ramos.i@gmail.com>
2026-03-14 12:06:52 +0100

    changes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,6 +11,8 @@ vim.pack.add ({
 		{ src="https://github.com/nvim-telescope/telescope.nvim"},
 		{ src="https://github.com/goolord/alpha-nvim" },
 		{ src="https://github.com/catppuccin/nvim", as = "catppuccin" },
+		{ src="https://github.com/neovim/nvim-lspconfig" },
+		{ src="https://github.com/hrsh7th/nvim-cmp" },
 })
 
 dofile(vim.fn.stdpath("config") .. "/alpha.lua")
@@ -40,3 +42,56 @@ local keymaps = {
 for _, k in ipairs(keymaps) do
 	vim.keymap.set(k[1], k[2], k[3])
 end
+
+-- C/C++
+-- Opciones de completado
+vim.o.completeopt = "menuone,noselect"
+
+-- Función para crear keymaps LSP en el buffer
+local function lsp_keymaps(bufnr)
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+end
+
+-- Configuración del cliente LSP
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "c", "cpp" },
+    callback = function()
+        local clients = vim.lsp.get_clients({ name = "clangd" })
+        if #clients == 0 then
+            vim.lsp.start({
+                name = "clangd",
+                cmd = { "clangd", "--background-index" },
+                filetypes = { "c", "cpp" },
+                root_dir = vim.loop.cwd(),
+            })
+        end
+
+        -- Asigna keymaps LSP para este buffer
+        lsp_keymaps(0)
+    end
+})
+
+-- Diagnósticos visibles
+vim.diagnostic.config({
+    virtual_text = true,  -- muestra errores como texto en la línea
+    signs = true,         -- muestra signos en la columna
+    underline = true,     -- subraya los errores
+    update_in_insert = true, -- actualiza en modo insert
+})
+local cmp = require('cmp')
+
+cmp.setup({
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    }
+})
