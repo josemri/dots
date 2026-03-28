dbfdaa17465bc90cfbbaff8c7888fcd64c43c76a
josep <josepm.ramos.i@gmail.com>
2026-03-28 11:35:53 +0100

    i3blocks

--- config/i3blocks/scripts/bluetooth.sh
+++ config/i3blocks/scripts/bluetooth.sh
@@ -2,10 +2,6 @@
 connected_device=$(bluetoothctl info | grep "Name" | awk -F ' ' '{print $2}')
 if [ -n "$connected_device" ]; then
     echo "bth:[$connected_device] " #󰂱
-else
-    echo ""
+	 echo
+	 echo "#ffffff"
 fi
-
-echo
-echo "#ffffff"
-
--- config/i3blocks/scripts/net.sh
+++ config/i3blocks/scripts/net.sh
@@ -3,6 +3,8 @@
 INTERFACE="enx00e04c68432a"
 if ip link show $INTERFACE | grep -q "state UP"; then
     SPEED=$(cat /sys/class/net/$INTERFACE/speed 2>/dev/null || echo "Unknown")
-    echo "<span background='#caffbf' font_weight='bold'>  </span><span background='#caffbf' font_weight='bold'>$SPEED Mbit/s </span>"
+    # echo "<span background='#caffbf' font_weight='bold'>  </span><span background='#caffbf' font_weight='bold'>$SPEED Mbit/s </span>"
+	 echo "eth:[$SPEED Mbit/s] "
+	 echo
+	 echo "#ffffff"
 fi
-
