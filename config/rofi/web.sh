ea2c376a630af13e0ebae3787e913c9351ef2f33
josep <josepm.ramos.i@gmail.com>
2026-04-17 11:40:08 +0200

    fix:minor changes

--- config/rofi/web.sh
+++ config/rofi/web.sh
@@ -9,6 +9,21 @@ declare -A LINKS=(
   ["youtube"]="https://youtube.com"
   ["claude"]="https://claude.ai/new"
 )
-choice=$(printf "%s\n" "${!LINKS[@]}" | rofi -dmenu -i -p "Web")
-[[ -n "$choice" ]] && xdg-open "${LINKS[$choice]}"
 
+if [[ -z "$1" ]]; then
+  printf "%s\n" "${!LINKS[@]}"
+  exit 0
+fi
+
+choice="$1"
+
+if [[ -n "${LINKS[$choice]}" ]]; then
+  xdg-open "${LINKS[$choice]}"
+  exit 0
+fi
+
+# 2. Si no existe → buscar en Firefox
+query=$(printf "%s" "$choice" | sed 's/ /+/g')
+
+firefox "https://duckduckgo.com/?q=$query" &
+exit 0
