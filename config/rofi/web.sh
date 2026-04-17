a12e85ac398c67661f14c50c43a59eeae180084d
josep <josepm.ramos.i@gmail.com>
2026-04-17 11:48:22 +0200

    web

--- config/rofi/web.sh
+++ config/rofi/web.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 declare -A LINKS=(
-  ["whatsapp"]="https://web.whatsapp.com"
-  ["discord"]="https://discord.com/app"
-  ["chatgpt"]="https://chat.openai.com"
-  ["campus"]="https://campusvirtual.uclm.es"
-  ["github"]="https://github.com"
-  ["youtube"]="https://youtube.com"
-  ["claude"]="https://claude.ai/new"
+	["whatsapp"]="https://web.whatsapp.com"
+	["discord"]="https://discord.com/app"
+	["chatgpt"]="https://chat.openai.com"
+	["campus"]="https://campusvirtual.uclm.es"
+	["github"]="https://github.com"
+	["youtube"]="https://youtube.com"
+	["claude"]="https://claude.ai/new"
 )
 
 if [[ -z "$1" ]]; then
-  printf "%s\n" "${!LINKS[@]}"
-  exit 0
+	printf "%s\n" "${!LINKS[@]}"
+	exit 0
 fi
 
 choice="$1"
 
 if [[ -n "${LINKS[$choice]}" ]]; then
-  xdg-open "${LINKS[$choice]}"
-  exit 0
+	setsid -f xdg-open "${LINKS[$choice]}" >/dev/null 2>&1
+	exit 0
 fi
 
 # 2. Si no existe → buscar en Firefox
 query=$(printf "%s" "$choice" | sed 's/ /+/g')
 
-firefox "https://duckduckgo.com/?q=$query" &
+setsid -f firefox "https://duckduckgo.com/?q=$query" >/dev/null 2>&1
 exit 0
